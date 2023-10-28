0089d6c3fb9acabab974e6ed1adfa912508a9165
Michael Robinson <robinson@Michaels-MBP.home>
2023-10-27 21:49:21 -0600

    Initial setup

---
+++ agility.go
@@ -0,0 +1,17 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+
+	fmt.Println("\nJust getting some text out to make sure this builds\n")
+
+	// take in input
+	// make the first call
+	// make subsequent second calls via batching or wait group
+	// combine data
+	// print
+
+}
